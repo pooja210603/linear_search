dedf252a1cb8cd61e81fbdb5270a8918e20c05b7
Pooja <125560933+pooja210603@users.noreply.github.com>
2023-04-17 20:06:49 +0530

    Update linear_search.cpp

--- linear_search.cpp
+++ linear_search.cpp
@@ -1,19 +1,33 @@
-#include<stdio.h>
-#include<string.h>
-int main()
-{
-	int arr[6]={9,6,7,8,2,10};
-	printf("Enter the number to be searched	: ");
-	int a,i;
-	scanf("%d",&a);
-	for (i=0;i<6;i++)
-	{
-		if (arr[i]==a)
-		{
-			return i+1;break;	
-		}
-	}
+#include <stdio.h>
+
+int linear_search(int arr[], int n, int target) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == target) {
+            return i; // Return the index if target found
+        }
+    }
+    return -1; // Return -1 if target not found
 }
+
+int main() {
+    int arr[] = {3, 8, 4, 1, 9, 5, 7};
+    int n = 7 // sizeof(arr) / sizeof(arr[0]);
+    int target = 5;
+    int result = linear_search(arr, n, target);
+    if (result == -1) {
+        printf("Target value not found in the array.\n");
+    } else {
+        printf("Target value found at index: %d\n", result);
+    }
+    return 0;
+}
+
+
+
+
+
+
+
 
 
 
