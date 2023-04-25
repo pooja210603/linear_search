dd4571716722e2544cc8c7161379f37992181197
Pooja <125560933+pooja210603@users.noreply.github.com>
2023-04-25 23:14:38 +0530

    Update linear_search.cpp

--- linear_search.cpp
+++ linear_search.cpp
@@ -11,7 +11,7 @@ int linear_search(int arr[], int n, int target) {
 
 int main() {
     int arr[] = {3, 8, 4, 1, 9, 5, 7};
-    int n = 7 // sizeof(arr) / sizeof(arr[0]);
+    int n = 7; // sizeof(arr) / sizeof(arr[0]);
     int target = 5;
     int result = linear_search(arr, n, target);
     if (result == -1) {
